a4002831f9c077547ea236fe3d932510f98ebdc3
workshophme2017 <31211163+workshophme2017@users.noreply.github.com>
2017-10-21 20:53:30 +0700

    Add files via upload

---
+++ Season 2/day 5/Programs/1webserver/webserver/webserver.ino
@@ -0,0 +1,45 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+
+// Create an instance of the server
+// specify the port to listen on as an argument
+// WiFi.printDiag(Serial);
+const char* ssid = "Lenovo";
+const char* password = "terserah";
+ESP8266WebServer server(80);
+
+void handleRoot() {
+  server.send(200, "text/html", "<h1>You are connected</h1>");
+}
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  delay(10);
+    
+  // Connect to WiFi network
+  Serial.println();  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  
+  // Start the server
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  server.handleClient();
+}
---
+++ Season 2/day 5/Programs/2WriteData/WriteData/WriteData.ino
@@ -0,0 +1,43 @@
+// include ThingSpeak and ESP8266 WiFi Libraries
+#include <ThingSpeak.h>
+#include <ESP8266WiFi.h>
+
+// set your Router user name and passward
+char ssid[] = "yashf";          //  your network SSID (name) 
+char pass[] = "12211221";   // your network password
+
+// that you have already remembered from ThingSpeak websie
+unsigned long myChannelNumber = 325405;
+const char * myWriteAPIKey = "PK3Q0KXF1RPK2G44";
+
+int sensorValue; // variable to save analog reading
+WiFiClient  client;
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  Serial.println("");
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  WiFi.printDiag(Serial);
+  ThingSpeak.begin(client); 
+}
+
+void loop() {
+  // read the input on analog pin 0:
+  sensorValue = analogRead(A0);
+  
+  // Write to ThingSpeak. 
+  // send your Channel Number
+  // write to field 1
+  // send sensorValue
+  // send your Write API Key
+  Serial.println(sensorValue);
+  ThingSpeak.writeField(myChannelNumber, 1, sensorValue, myWriteAPIKey);
+  delay(10000); // ThingSpeak will accept updates every 10 seconds.
+}
